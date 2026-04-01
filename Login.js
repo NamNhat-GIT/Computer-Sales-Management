0084776ba802f98ac0bb0d354c80f6484e02c6a3
Nam Nhat <nguyenphannhatnam2006@gmail.com>
2026-04-01 22:15:18 +0700

    login

--- Login.js
+++ Login.js
@@ -2,14 +2,15 @@ function LoginButton() {
     const email = document.querySelector('input[type="email"]').value.trim();
     const password = document.querySelector('input[type="password"]').value.trim();
 
-    const storedUser = JSON.parse(localStorage.getItem('user'));
+    const storedUser = JSON.parse(localStorage.getItem('user') || '[]');
 
-    if (storedUser) {
-        if (storedUser.email === email && storedUser.password === password) {
-            alert('Login successful!');
-            window.location.href = 'ProfilePage.html';
-        } else {
-            alert('Invalid email or password.');
-        }
+    let loginUser = storedUser.find(user => user.email === email && user.password === password);
+
+    if (loginUser) {
+        alert('Login successful!');
+        localStorage.setItem('currentUser', JSON.stringify(loginUser));
+        window.location.href = 'ProfilePage.html';
+    } else {
+        alert('Invalid email or password. Please try again.');
     }
 }
--- ProfilePage.js
+++ ProfilePage.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    let userData = JSON.parse(localStorage.getItem('user'));
+    let userData = JSON.parse(localStorage.getItem('user') || '[]').find(user => user.email === JSON.parse(localStorage.getItem('currentUser')).email);
     if (userData) {
         document.getElementById('userName').textContent = userData.fullName;
         document.getElementById('userEmail').textContent = userData.email;
